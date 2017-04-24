fc25dec8eea6e3a53b6490e2f48d73541719aee9
platzr01 <rplatz@sjm.com>
2017-04-24 06:46:14 +0000

    added motd based on node attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,9 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+if node['platform_family'] == "rhel"
+	package = "http"
+
+end
 
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,5 +1,5 @@
 file 'default www' do
-	path '/var/www/html/index/html'
+	path '/var/www/html/index.html'
 	content 'I\'m completely operational, and all my circuits are functioning perfectly'
 end
 
